624a11231df74f6088e177267633fb7de4b371c0
hwk <mail@h-wk.net>
2023-02-07 15:09:39 +0100

    Endstand wahlweise rekursiv nur neue Datensätze oder neuere Datensätze

--- controller.py
+++ controller.py
@@ -7,24 +7,35 @@ import database_sqlite
 import database
 
 database.create_tables()
-
-chronicle_dir = "C:\Daten\Desktop_20190912"
-chronicle_dir = "C:\Daten\Chronik_20221202"
-chronik = pathlib.Path(chronicle_dir)
-
 database_sqlite.create_tables()
 
+chronicle_dir = "C:\Daten\Desktop_20190912"
+#chronicle_dir = "C:\Daten\Chronik_20221202"
+chronik = pathlib.Path(chronicle_dir)
+
 list1 = []
+
 for item in chronik.iterdir():
+# for item in chronik.glob('**/*'):     # rekursiv in Unterverz.
     if item.is_file():
+        filepath = str(item.parent)
         filename = pathlib.Path(item).stem
         fileextension = pathlib.Path(item).suffix[1:]
         filesize = item.stat().st_size
         dt = datetime.fromtimestamp(item.stat().st_mtime)
         str_date_time = dt.strftime("%d.%m.%Y, %H:%M:%S")
         timestamp = item.stat().st_mtime
-        t1 = (chronicle_dir, filename, fileextension, filesize, str_date_time, timestamp)
-        list1.append(t1)
+        rt = database.search_chronicle_item(chronicle_dir, filename)
+        if rt:
+            if rt[5] < timestamp:
+                database.delete_chronicle_item(rt[0])
+                t1 = (filepath, filename, fileextension, filesize, str_date_time, timestamp)
+                list1.append(t1)
+        else:
+            t1 = (filepath, filename, fileextension, filesize, str_date_time, timestamp)
+            list1.append(t1)
+
 
 database.create_chronicle_item(list1)
-database_sqlite.create_chronical_item(list1)
+#database_sqlite.create_chronical_item(list1)
+
--- database.py
+++ database.py
@@ -15,7 +15,14 @@ CREATE_CHRONICLE_ITEM_TABLE = """CREATE TABLE IF NOT EXISTS chronicle_item (
     item_source INTEGER    
 );"""
 
-INSERT_CHRONICLE_ITEM = "INSERT INTO chronicle_item (root_path, item_name, file_extension, file_size, file_datetime, file_datetime_timestamp) VALUES %s;"         # %s => Tupel!
+CREATE_CHRONICLE_ITEM_TABLE_INDEX = """CREATE INDEX IF NOT EXISTS idx_chronicle_item ON  chronicle_item (root_path, item_name);"""
+
+
+INSERT_CHRONICLE_ITEM = """INSERT INTO chronicle_item (root_path, item_name, file_extension, file_size, file_datetime, file_datetime_timestamp) VALUES %s;"""         # %s => Tupel!
+
+SEARCH_CHRONICAL_ITEM = """SELECT * FROM chronicle_item WHERE root_path = %s AND item_name = %s;"""
+
+DELETE_CHRONICLE_ITEM = """DELETE FROM chronicle_item WHERE id = %s;"""
 
 load_dotenv()
 
@@ -31,9 +38,23 @@ def create_tables():
     with connection:
         with connection.cursor() as cursor:
             cursor.execute(CREATE_CHRONICLE_ITEM_TABLE)
+            cursor.execute(CREATE_CHRONICLE_ITEM_TABLE_INDEX)
 
 def create_chronicle_item(options):
     with connection:
         with connection.cursor() as cursor:
             chronical_values = [x for x in options]
             execute_values(cursor, INSERT_CHRONICLE_ITEM, chronical_values)
+
+def search_chronicle_item(root_path, item):
+    #root_path = "C:\Daten\Chronik_20221202"
+    #item = "Dok25"
+    with connection:
+        with connection.cursor() as cursor:
+            cursor.execute(SEARCH_CHRONICAL_ITEM, (root_path,item,))
+            return cursor.fetchone()
+
+def delete_chronicle_item(_id):
+    with connection:
+        with connection.cursor() as cursor:
+            cursor.execute(DELETE_CHRONICLE_ITEM, (_id,))
